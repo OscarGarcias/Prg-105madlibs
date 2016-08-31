727aa0895b749618582888e1f0a324981656c8a5
OscarGarcias <og031197@gmail.com>
2016-08-31 08:46:18 -0500

    Add files via upload

---
+++ nursery.py
@@ -0,0 +1,33 @@
+#variables
+number = "ten"
+Person= "father"
+pet = "bird"
+action = "smile"
+emotion ="joy"
+Clothes = "pants"
+food = "apples"
+action2 = "bark"
+
+# poem
+print(number + " little " + pet + ",")
+print("they lost their " + clothes + ",")
+print("And they began to " + action + ",")
+print("We sadly " + emotion + ",")
+print("Our " +  + " we have lost. "
+print("What! Lost your " + clothes + ",")
+print("You naughty " + pet + "!")
+print("Then you shall have no " + food + ".")
+print(action2 "," + action2 "," + action2 "," + action2 ",")
+print("You shall have no " + food + ".\n)
+
+#result
+Ten little birds,
+They lost their pants,
+And they began to smile,
+We sadly joy,
+Our pants we have lost.
+What! Lost your pants,
+You naughty birds!
+Then you shall have no apples.
+Bark, bark, bark, bark,
+You shall have no apples.
